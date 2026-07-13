e77b7abf70920f2cf7e93c423b73b2d0d4d52192
dilshod-yusupov <dilshod.yusupov04@icloud.com>
2026-07-13 19:26:15 +0400

    Исправиль ошибки в JS файле

--- homework-7.js
+++ homework-7.js
@@ -1,20 +1,19 @@
 function printWeather(cityName, temperature) {
-console.log(`В Городе ${cityName} сейчас ${temperature} градусов`);
+  console.log(`Сейчас в ${cityName} ${temperature} градусов по Цельсию`);
 }
 // Тестируем логику:
-printWeather('Москва', 20);
-printWeather('Санкт-Петербург', 15);
+printWeather("Москва", 20);
+printWeather("Санкт-Петербург", 15);
 
 const SPEED_OF_LIGHT = 299792458;
 function printSpeedOfLight(currentSpeed) {
-
-if (currentSpeed > SPEED_OF_LIGHT) {
-console.log("Сверхсветовая скорость");
-} else if (currentSpeed === SPEED_OF_LIGHT) {
-console.log("Скорость света");
-} else {
-console.log("Субсветовая скорость");
-} 
+  if (currentSpeed > SPEED_OF_LIGHT) {
+    console.log("Сверхсветовая скорость");
+  } else if (currentSpeed === SPEED_OF_LIGHT) {
+    console.log("Скорость света");
+  } else {
+    console.log("Субсветовая скорость");
+  }
 }
 // Тестируем логику:
 printSpeedOfLight(300000000);
@@ -24,29 +23,35 @@ printSpeedOfLight(1000000);
 const productName = "iPhone 14 Pro";
 const productPrice = 999;
 function tryBuyProduct(userBudget) {
-    if (userBudget >= productPrice) {
-        console.log(`${productName} приобретён. Спасибо за покупку!`);
-    } else {
-        const debt = productPrice - userBudget;
-        console.log(`Вам не хватает ${debt}, пополните баланс`);    
-    }
-    }
+  if (userBudget >= productPrice) {
+    console.log(`${productName} приобретён. Спасибо за покупку!`);
+  } else {
+    const debt = productPrice - userBudget;
+    console.log(`Вам не хватает ${debt}$, пополните баланс`);
+  }
+}
 // Тестируем логику:
 tryBuyProduct(1000);
 tryBuyProduct(500);
 
 const CRITICAL_FUEL_LEVEL = 10;
 function fillGasTank(fuelType, tankCapacity, currentFuelLevel) {
-
-if (currentFuelLevel < CRITICAL_FUEL_LEVEL) {
+  if (currentFuelLevel < CRITICAL_FUEL_LEVEL) {
     const fuelNeeded = tankCapacity - currentFuelLevel;
-    console.log(`Критический уровень топлива! До полного бака нужно залить еще ${fuelNeeded} л.`);
-} else if (currentFuelLevel >= CRITICAL_FUEL_LEVEL && currentFuelLevel < tankCapacity) {
+    console.log(
+      `Критический уровень топлива! До полного бака нужно залить еще ${fuelNeeded} л.`,
+    );
+  } else if (
+    currentFuelLevel >= CRITICAL_FUEL_LEVEL &&
+    currentFuelLevel < tankCapacity
+  ) {
     const fuelNeeded = tankCapacity - currentFuelLevel;
-    console.log(`Уровень топлива в норме. В бак поместится еще ${fuelNeeded} л.`);
-} else {
-    console.log('Бак полон! Заправка не нужна.');
-}
+    console.log(
+      `Уровень топлива в норме. В бак поместится еще ${fuelNeeded} л.`,
+    );
+  } else {
+    console.log("Бак полон! Заправка не нужна.");
+  }
 }
 // Добавляем 3 новые переменные (Задание 7):
 const testFuelType = "АИ-95";
@@ -57,4 +62,4 @@ fillGasTank(testFuelType, testCapacity, testFuelLevel);
 
 // Тестируем логику:
 fillGasTank("АИ-95", 60, 40);
-fillGasTank("Дизель", 70, 70);  
+fillGasTank("Дизель", 70, 70);
